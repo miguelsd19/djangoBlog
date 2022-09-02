056a9af6057fafd7ee1f8f9fe7da2a87674f0b2a
Miguel Sánchez Domínguez <miguelsd@Miguels-MacBook-Pro.local>
2022-09-02 18:18:41 -0500

    Se añade el swagger para documentacion

--- blog/settings.py
+++ blog/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'django.contrib.staticfiles',  # required for serving swagger ui's css/js files
+    'rest_framework', 
     'drf_yasg',
     'users',
 ]
@@ -120,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT= './static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
