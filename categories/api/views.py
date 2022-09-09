47555e5a153d0d6952b062f733cd5463df8ba2b3
Miguel Sánchez Domínguez <miguelsd@Miguels-MacBook-Pro.local>
2022-09-08 23:01:56 -0500

    Se agregan permisos al CRUD de categories y se cambian los endpoint unicos de busqueda por id a slug

--- categories/api/views.py
+++ categories/api/views.py
@@ -1,8 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from categories.models import Category
 from categories.api.serializers import CategorySerializer
+from categories.api.permissions import IsAdminOrReadOnly
 
 
 class CategoryApiViewSet(ModelViewSet):
-    serializer_class = CategorySerializer()
+    permission_classes = [IsAdminOrReadOnly]
+    serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    lookup_field = 'slug'
--- categories/models.py
+++ categories/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=255)
-    slugs = models.SlugField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
     published = models.BooleanField(default=False)
 
     def __str__(self):
