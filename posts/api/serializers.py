9306424999645f314066bbcd0bf065f24911d8d7
Miguel Sánchez Domínguez <miguelsd@Miguels-MacBook-Pro.local>
2022-09-14 14:56:14 -0500

    Se agrega el retorno de los datos del usurio en los posts

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from posts.models import Post
+from users.api.serializer import UserSerializer
 
 
 class Postserializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Post
-        fields = ['title', 'content', 'slug', 'miniature', 'created_at', 'published', 'category']
+        fields = ['title', 'content', 'slug', 'miniature', 'created_at', 'user', 'published', 'category']
