824eef69829c4797a29bf0afe804da6a411c792d
Miguel Sánchez Domínguez <miguelsd@Miguels-MacBook-Pro.local>
2022-09-14 14:58:09 -0500

    Se agrega retorno de datos de category en los posts

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from posts.models import Post
 from users.api.serializer import UserSerializer
+from categories.api.serializers import CategorySerializer
 
 
 class Postserializer(serializers.ModelSerializer):
+    category = CategorySerializer()
     user = UserSerializer()
 
     class Meta:
